da2742f9aa47e007197573a12882c8d54146bcc8
TuncayHp2 <tunc270@gmail.com>
2026-05-20 08:17:21 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 export { ExampleButton } from "./ExampleButton";
 
-//export {Fel} from "./fiDataContainer/Fel";
-
-export function greet(name: string): string {
-    //let a = new FiKeyEntity()
-    return `Hello, ${name}!`;
-}
