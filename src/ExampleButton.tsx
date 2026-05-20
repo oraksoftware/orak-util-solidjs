4925324aa167aa90ed00b52c7c66ccc6d0d5fefc
TuncayHp2 <tunc270@gmail.com>
2026-05-20 12:12:57 +0300

    publish yapıldı

--- src/ExampleButton.tsx
+++ src/ExampleButton.tsx
@@ -1,5 +1,6 @@
 export interface ExampleButtonProps {
   label?: string;
+  // onClick opsiyonel fonksiyon, bir Runnable Function 
   onClick?: () => void;
   class?: string;
 }
@@ -10,7 +11,8 @@ export const ExampleButton = (props: ExampleButtonProps) => {
     <button
       type="button"
       class={props.class}
-      //|| "px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 focus:outline-none
+      // props.class || "px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 focus:outline-none
+      // onClick fonksiyonu = props.onClick fonksiyonu
       onClick={props.onClick}
     >
       {props.label ?? "Click me"}
